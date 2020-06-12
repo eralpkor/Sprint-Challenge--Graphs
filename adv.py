533ef7644eb89cdc32d10e42f816b3b66f779520
eralpkor <eralp.ny@gmail.com>
2020-06-12 15:47:26 -0400

    Cleaned code

--- adv.py
+++ adv.py
@@ -12,11 +12,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -31,53 +31,8 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 # -----------------------------------------------------------------------------------------
-# Traverse maze in a DFT
-# def build_path(graph):
-#     # stack contains our current path
-#     stack = Stack()
-#     # List that contains returned path
-#     moves = []
-#     # to find out if we hit a dead end
-#     visited = set()
-#     # start traversal with 0 index room
-#     stack.push(0)
 
-#     while len(visited) < len(graph):
-#         # get the current room id in the stack
-#         current_room_id = stack.tail()
-#         # mark it as visited
-#         visited.add(current_room_id)
-#         # Get info on the current room (tuple data)
-#         current_room = graph[current_room_id]
-#         # dict of possible moves:
-#         rooms_dict = current_room[1]
-#         # List (array) to track if room has not been visited yet
-#         unvisited = []
-#         # store unvisited rooms in relation to the current room
-#         for direction, room_id in rooms_dict.items():
-#             if room_id not in visited:
-#                 unvisited.append(room_id)
-
-#         # assign next room
-#         # if reached a dead end, back track
-#         if len(unvisited) > 0:
-#             next_room = unvisited[0]
-#             stack.push(next_room)
-#         else:
-#             stack.pop()
-#             next_room = stack.tail()
-
-#         # check the rooms around the current room. if the next move matches
-#         # room_id add it to moves and walk
-#         for directions, adjacent_id in rooms_dict.items():
-#             if adjacent_id == next_room:
-#                 moves.append(direction)
-            
-#     return moves
-
-# traversal_path = build_path(room_graph)
-
-# move the player:
+# move the player directions
 def move_player(direction):
     player.travel(direction) # check players direction options
     # keep adding to traversal path
@@ -116,8 +71,9 @@ def DFT_recursive(visited=None, prev=None, move=None):
 
 DFT_recursive() # invoke function with default (3*None)
 
-# print("TRAVERSAL PATH: ", traversal_path)
+print("TRAVERSAL PATH: ", traversal_path)
 # -----------------------------------------------------------------------------------------
+
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
